81544bc6b156055ff2988e0a06d900473f1b7c25
GoldenMEmre <goldenpie60@gmail.com>
2023-03-29 00:55:57 +0300

    29032023

--- src/C07.java
+++ src/C07.java
@@ -2,5 +2,7 @@ public class C07 {
     public static void main(String[] args) {
 
         System.out.println("3. Gün Merhaba Github");
+        System.out.println("==============");
+        System.out.println("Have a wise day");
     }
 }
